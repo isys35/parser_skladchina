8e33f605030c7d828be36dcfd5887662d103d6bd
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-02-26 23:13:58 +0300

    final

--- db.py
+++ db.py
@@ -1,29 +1,46 @@
 import openpyxl
 import os
+from datetime import datetime
 
+HEADER = ['Категория курса', 'Статус курса', 'Дата создания темы', 'Название темы',
+          'Количество просмотров', 'Количество просмотров деленное на количестство дней на сайте',
+          'Цена', 'Взнос', 'Цена деленная на взнос', 'Количество Складчиков в основном списке',
+          'Количество просмотров деленное на количество складчиков', 'Хэштеги']
 
 
 def create_file(file_name):
     wb = openpyxl.Workbook()
+    ws = wb.active
+    for column in range(1, len(HEADER) + 1):
+        ws.cell(row=1, column=column).value = HEADER[column - 1]
     wb.save(file_name)
 
 
 def add_skladchina(data: dict, file_name):
     if not os.path.isfile(file_name):
-        wb = openpyxl.Workbook()
-        max_row = 0
-        ws = wb.active
-    else:
-        wb = openpyxl.load_workbook(file_name)
-        ws = wb.active
-        max_row = ws.max_row
-    ws.cell(row=max_row+1, column=1).value = data['rubric']
-    ws.cell(row=max_row+1, column=2).value = data['status']
-    ws.cell(row=max_row+1, column=3).value = data['name']
-    ws.cell(row=max_row+1, column=4).value = data['date']
-    ws.cell(row=max_row+1, column=5).value = data['price']
-    ws.cell(row=max_row+1, column=6).value = data['deposit']
-    ws.cell(row=max_row+1, column=7).value = data['main']
-    #ws.cell(row=max_row+1, column=8).value = data['rezerve']
-    ws.cell(row=max_row+1, column=8).value = data['views']
+        create_file(file_name)
+    wb = openpyxl.load_workbook(file_name)
+    ws = wb.active
+    max_row = ws.max_row
+    ws.cell(row=max_row + 1, column=1).value = data['rubric']
+    ws.cell(row=max_row + 1, column=2).value = data['status']
+    ws.cell(row=max_row + 1, column=3).value = data['date'].strftime("%d.%m.%Y")
+    ws.cell(row=max_row + 1, column=4).value = data['name']
+    views = float(data['views'].replace('.', ''))
+    ws.cell(row=max_row + 1, column=5).value = views
+    days_on_site = (datetime.now()-data['date']).days
+    ws.cell(row=max_row + 1, column=6).value = views/days_on_site
+    price = float(data['price'].replace(' руб', ''))
+    ws.cell(row=max_row + 1, column=7).value = price
+    deposit = float(data['deposit'].replace(' руб', ''))
+    ws.cell(row=max_row + 1, column=8).value = deposit
+    ws.cell(row=max_row + 1, column=9).value = price/deposit
+    main = float(data['main'].replace('.', ''))
+    ws.cell(row=max_row + 1, column=10).value = main
+    ws.cell(row=max_row + 1, column=11).value = views/main
+    ws.cell(row=max_row + 1, column=12).value = ', '.join(data['hash_tags'])
     wb.save(file_name)
+
+
+if __name__ == '__main__':
+    create_file('data.xlsx')
--- parser_skadchina.py
+++ parser_skadchina.py
@@ -2,11 +2,123 @@ import requests
 from bs4 import BeautifulSoup
 from typing import List
 import db
+from requests import Session
+from urllib.parse import quote
+from datetime import datetime
 
 HOST = 'https://www.skladchina.biz/'
+HOST_2 = 'https://tor.skladchina.biz/'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'
 }
+HEADERS_LOGIN = {
+    'Host': 'www.skladchina.biz',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': '93',
+    'Origin': 'https://www.skladchina.biz',
+    'Cookie': '_ym_uid=1614277842861231321',
+    'Connection': 'keep-alive',
+    'Referer': 'https://www.skladchina.biz/',
+    'Upgrade-Insecure-Requests': '1',
+    'TE': 'Trailers'
+}
+
+HEADERS_SIGN_UP = {
+    'Host': 'www.skladchina.biz',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
+    'Accept': '*/*',
+    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'X-Requested-With': 'XMLHttpRequest',
+    'Content-Length': '67',
+    'Origin': 'https://www.skladchina.biz',
+    'Connection': 'keep-alive',
+    'TE': 'Trailers'
+}
+
+HEADERS_REKVIZIT = {
+    'Host': 'www.skladchina.biz',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'TE': 'Trailers'
+}
+
+RU_MONTH_VALUES = {
+    'янв': 1,
+    'фев': 2,
+    'мар': 3,
+    'апр': 4,
+    'май': 5,
+    'июн': 6,
+    'июл': 7,
+    'авг': 8,
+    'сен': 9,
+    'окт': 10,
+    'ноя': 11,
+    'дек': 12,
+}
+
+session = Session()
+
+
+def int_value_from_ru_month(date_str):
+    for k, v in RU_MONTH_VALUES.items():
+        date_str = date_str.replace(k, str(v))
+    return date_str
+
+
+# def login():
+#     url = 'https://www.skladchina.biz/login/login'
+#     data = 'login={}&password={}&remember=1&cookie_check=1&redirect=%2F&_xfToken='
+#     data = data.format(quote(config.EMAIL), quote(config.PASSWORD))
+#     session.post(url, data=data, headers=HEADERS_LOGIN)
+
+
+def sign_up(url, token, referer):
+    """
+    Запись на складчину
+    """
+    headers = HEADERS_SIGN_UP
+    headers['Referer'] = referer
+    session.headers.update(headers)
+    data = '_xfToken={}'.format(token)
+    response = session.post(url, data=data)
+    return response.text
+
+
+def leave(url, token, referer):
+    """
+    Отписка от складчины
+    """
+    sign_up(url, token, referer)
+
+
+def parse_rekvizit_url(response: str) -> str or None:
+    """
+    Парсинг ссылки на реквизиты
+    """
+    soup = BeautifulSoup(response, 'lxml')
+    btn = soup.select_one('#LeaveButton')
+    if not btn:
+        return
+    if not btn.select_one('a'):
+        return
+    return HOST + btn.select_one('a')['href']
+
+
+def parse_leave_url(response: str) -> str:
+    soup = BeautifulSoup(response, 'lxml')
+    btn = soup.select_one('a.button-new-primary.estcs-button')
+    return HOST + btn['href']
 
 
 def save_page(response: str, file_name='page.html'):
@@ -53,6 +165,15 @@ def parse_max_page(response: str) -> int:
     return int(soup.select_one('div.PageNav')['data-last'])
 
 
+def transform_date(init_date: str) -> datetime:
+    date_str = int_value_from_ru_month(init_date)
+    if 'в' in date_str:
+        date = datetime.strptime(date_str, '%d %m %Y в %H:%M')
+    else:
+        date = datetime.strptime(date_str, '%d %m %Y')
+    return date
+
+
 def parse_skladchini(response: str) -> List[dict]:
     """
     Парсер складчин с ответа на запрос
@@ -68,9 +189,9 @@ def parse_skladchini(response: str) -> List[dict]:
             name = list_item.select_one('a.PreviewTooltip').text
             url = HOST + list_item.select_one('a.PreviewTooltip')['href']
             date = list_item.select_one('.startDate').select_one('.DateTime').text
+            date = transform_date(date)
             deposit = list_item.select_one('.estcs-shopping-description').select_one('dd').text
             main = list_item.select_one('dl.major').select_one('dd').text
-            rezerve = None
             views = list_item.select_one('dl.minor').select_one('dd').text
             skladchini.append({'status': status,
                                'name': name,
@@ -78,7 +199,6 @@ def parse_skladchini(response: str) -> List[dict]:
                                'date': date,
                                'deposit': deposit,
                                'main': main,
-                               #'rezerve': rezerve,
                                'views': views
                                })
     return skladchini
@@ -89,9 +209,13 @@ def parse_skladchina(response: str) -> dict:
     Парсер складчины с ответа на запрос
     склачина - {'наименованиее данных':'данные'}
     """
+    save_page(response)
     soup = BeautifulSoup(response, 'lxml')
     price = soup.select_one('dl.estcs-shopping-info-extra-tabs').select_one('dd').text
-    return {'price': price}
+    hash_tags = [el.text for el in soup.select('.link.tag')]
+    # zapis_url = HOST + soup.select('.but_zapis')[-1].select_one('a')['href']
+    # token = soup.find("input", {"name": "_xfToken"})['value']
+    return {'price': price, 'hash_tags': hash_tags}  # {'price': price, 'zapis_url': zapis_url, 'xsrf_token': token}
 
 
 def get_rubrics() -> List[dict]:
@@ -121,6 +245,16 @@ def get_skladchini(rubric_url: str) -> List[dict]:
     return skladchini
 
 
+# def get_rekvizit_count_messages(url) -> int:
+#     """
+#     Получение кол-ва сообщений в реквизитах
+#     """
+#     session.headers = {}
+#     login()
+#     response = get_response(url)
+#     save_page(response)
+
+
 def get_skladchina(skladchina_url: str) -> dict:
     """
     Получение данных со страницы складчины
@@ -128,24 +262,49 @@ def get_skladchina(skladchina_url: str) -> dict:
     """
     response = get_response(skladchina_url)
     skladchina = parse_skladchina(response)
+    # rekvizit_url = parse_rekvizit_url(sign_response)
+    # print(rekvizit_url)
+
     return skladchina
 
 
-def parser():
+def get_rubric_name(rubric_url: str) -> str:
+    response = get_response(rubric_url)
+    soup = BeautifulSoup(response, 'lxml')
+    return soup.select_one('h1').text
+
+
+def parser(url_rubric, rubric_name):
+    skladchini = get_skladchini(url_rubric)
+    for index, skladchina in enumerate(skladchini):
+        skladchina_detail = get_skladchina(skladchina['url'])
+        skladchina['rubric'] = rubric_name
+        skladchina_full = {**skladchina, **skladchina_detail}
+        print('{} {}/{}'.format(rubric_name, index, len(skladchini)))
+        db.add_skladchina(skladchina_full, rubric_name + '.xlsx')
+
+
+def parser_1():
     """
-    ПАРСЕР
+    Парсер 1-й режим:
+    парсер всего форума
     """
     rubrics = get_rubrics()
     for rubric in rubrics:
         db.create_file(rubric['name'] + '.xlsx')
-        skladchini = get_skladchini(rubric['url'])
-        for index, skladchina in enumerate(skladchini):
-            skladchina_detail = get_skladchina(skladchina['url'])
-            skladchina['rubric'] = rubric['name']
-            skladchina_full = {**skladchina, **skladchina_detail}
-            print('{} {}/{}'.format(rubric['name'], index, len(skladchini)))
-            db.add_skladchina(skladchina_full, rubric['name'] + '.xlsx')
+        parser(rubric['url'], rubric['name'])
+
+
+def parser_2():
+    """
+    Парсер 2-й режим:
+    парсер одной рубрики по ссылке
+    """
+    url_rubric = input('Введите url рубрики: ')
+    rubric_name = get_rubric_name(url_rubric)
+    db.create_file(rubric_name + '.xlsx')
+    parser(url_rubric, rubric_name)
 
 
 if __name__ == '__main__':
-    parser()
+    parser_2()
