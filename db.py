6fd75e7723a7863b1b9eaf6a5ac02ac04bbfab73
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-03-09 21:28:15 +0300

    finish

--- db.py
+++ db.py
@@ -2,26 +2,22 @@ import openpyxl
 import os
 from datetime import datetime
 
-HEADER = ['Категория курса', 'Статус курса', 'Дата создания темы', 'Название темы',
-          'Количество просмотров', 'Количество просмотров деленное на количестство дней на сайте',
-          'Цена', 'Взнос', 'Цена деленная на взнос', 'Количество Складчиков в основном списке',
-          'Количество просмотров деленное на количество складчиков', 'Хэштеги']
-
 
 class Excel:
     def __init__(self):
-        self.header = ['Категория курса', 'Статус курса', 'Дата создания темы', 'Название темы',
-                       'Количество просмотров', 'Количество просмотров деленное на количестство дней на сайте',
-                       'Цена', 'Взнос', 'Цена деленная на взнос', 'Количество Складчиков в основном списке',
-                       'Количество просмотров деленное на количество складчиков', 'Хэштеги']
+        self.header = ['Ссылка', 'Категория курса', 'Статус курса', 'Дата создания темы', 'Название темы',
+                                'Количество просмотров', 'Количество просмотров деленное на количестство дней на сайте',
+                                'Цена', 'Взнос', 'Цена деленная на взнос', 'Количество Складчиков в основном списке',
+                                'Количество просмотров деленное на количество складчиков', 'Хэштеги',
+                                'Cреднее количество участников в день']
         self.file_name = 'data.xlsx'
         self.wb = None
 
     def create_file(self):
         self.wb = openpyxl.Workbook()
         ws = self.wb.active
-        for column in range(1, len(HEADER) + 1):
-            ws.cell(row=1, column=column).value = HEADER[column - 1]
+        for column in range(1, len(self.header) + 1):
+            ws.cell(row=1, column=column).value = self.header[column - 1]
         self.wb.save(self.file_name)
 
     def save(self):
@@ -34,23 +30,35 @@ class Excel:
             self.wb = openpyxl.load_workbook(self.file_name)
         ws = self.wb.active
         max_row = ws.max_row
-        ws.cell(row=max_row + 1, column=1).value = data['rubric']
-        ws.cell(row=max_row + 1, column=2).value = data['status']
-        ws.cell(row=max_row + 1, column=3).value = data['date'].strftime("%d.%m.%Y")
-        ws.cell(row=max_row + 1, column=4).value = data['name']
+        ws.cell(row=max_row + 1, column=1).value = data['url']
+        ws.cell(row=max_row + 1, column=2).value = data['rubric']
+        ws.cell(row=max_row + 1, column=3).value = data['status']
+        ws.cell(row=max_row + 1, column=4).value = data['date'].strftime("%d.%m.%Y")
+        ws.cell(row=max_row + 1, column=5).value = data['name']
         views = float(data['views'].replace('.', ''))
-        ws.cell(row=max_row + 1, column=5).value = views
+        ws.cell(row=max_row + 1, column=6).value = views
         days_on_site = (datetime.now() - data['date']).days
         try:
-            ws.cell(row=max_row + 1, column=6).value = views / days_on_site
+            ws.cell(row=max_row + 1, column=7).value = views / days_on_site
         except ZeroDivisionError:
-            ws.cell(row=max_row + 1, column=6).value = 'Деление на 0'
+            ws.cell(row=max_row + 1, column=7).value = 'Деление на 0'
         price = float(data['price'].replace(' руб', ''))
-        ws.cell(row=max_row + 1, column=7).value = price
+        ws.cell(row=max_row + 1, column=8).value = price
         deposit = float(data['deposit'].replace(' руб', ''))
-        ws.cell(row=max_row + 1, column=8).value = deposit
-        ws.cell(row=max_row + 1, column=9).value = price / deposit
+        ws.cell(row=max_row + 1, column=9).value = deposit
+        ws.cell(row=max_row + 1, column=10).value = price / deposit
         main = float(data['main'].replace('.', ''))
-        ws.cell(row=max_row + 1, column=10).value = main
-        ws.cell(row=max_row + 1, column=11).value = views / main
-        ws.cell(row=max_row + 1, column=12).value = ', '.join(data['hash_tags'])
+        ws.cell(row=max_row + 1, column=11).value = main
+        try:
+            ws.cell(row=max_row + 1, column=12).value = views / main
+        except ZeroDivisionError:
+            ws.cell(row=max_row + 1, column=12).value = 'Деление на 0'
+        ws.cell(row=max_row + 1, column=13).value = ', '.join(data['hash_tags'])
+        if data['event']['count'] == 0:
+            averape_people_per_day = 0
+        else:
+            try:
+                averape_people_per_day = data['event']['count'] / (datetime.now() - data['event']['date']).days
+            except ZeroDivisionError:
+                averape_people_per_day = 0
+        ws.cell(row=max_row + 1, column=14).value = averape_people_per_day
--- parser_skadchina.py
+++ parser_skadchina.py
@@ -1,13 +1,13 @@
-import requests
 from bs4 import BeautifulSoup
 from typing import List
 import db
 from requests import Session
+from urllib.parse import quote
 from datetime import datetime
-import traceback
+import re
 
 HOST = 'https://www.skladchina.biz/'
-HOST_2 = 'https://tor.skladchina.biz/'
+
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'
 }
@@ -76,6 +76,13 @@ def int_value_from_ru_month(date_str):
     return date_str
 
 
+def login(email, password):
+    url = 'https://www.skladchina.biz/login/login'
+    data = 'login={}&password={}&remember=1&cookie_check=1&redirect=%2F&_xfToken='
+    data = data.format(quote(email), quote(password))
+    session.post(url, data=data, headers=HEADERS_LOGIN)
+
+
 def sign_up(url, token, referer):
     """
     Запись на складчину
@@ -128,7 +135,7 @@ def get_response(url: str) -> str:
     """
     while True:
         try:
-            response = requests.get(url, headers=HEADERS)
+            response = session.get(url)
             break
         except ConnectionError:
             continue
@@ -155,7 +162,35 @@ def parse_rubrics(response: str) -> List[dict]:
 
 def parse_max_page(response: str) -> int:
     soup = BeautifulSoup(response, 'lxml')
-    return int(soup.select_one('div.PageNav')['data-last'])
+    page_nav = soup.select_one('div.PageNav')
+    if page_nav:
+        return int(soup.select_one('div.PageNav')['data-last'])
+    else:
+        return 1
+
+
+def parse_event(response: str) -> dict:
+    """
+    Парсинг событий
+    :return {'count' : кол-во участвующих, 'date': дата первого события}
+    """
+    soup = BeautifulSoup(response, 'lxml')
+    scroller = soup.select_one('.alerts.alertsScroller')
+    count = 0
+    for li in scroller.select('li'):
+        if re.search('участвует в складчине в резервном списке', str(li)):
+            continue
+        if re.search('участвует в складчине', str(li)):
+            count += 1
+    if scroller.select('li'):
+        try:
+            first_date = scroller.select('li')[-1].select_one('.DateTime')['title']
+        except KeyError:
+            first_date = scroller.select('li')[-1].select_one('.DateTime')['data-datestring']
+        first_date = transform_date(first_date)
+        return {'count': count, 'date': first_date}
+    else:
+        return {'count': 0, 'date': datetime.now()}
 
 
 def transform_date(init_date: str) -> datetime:
@@ -176,6 +211,8 @@ def parse_skladchini(response: str) -> List[dict]:
     list_items = soup.select('.discussionListItem')
     skladchini = []
     for list_item in list_items:
+        if 'sticky' in list_item.attrs['class']:
+            continue
         prefix_link = list_item.select_one('.prefixLink')
         if prefix_link:
             status = prefix_link.text
@@ -205,7 +242,9 @@ def parse_skladchina(response: str) -> dict:
     soup = BeautifulSoup(response, 'lxml')
     price = soup.select_one('dl.estcs-shopping-info-extra-tabs').select_one('dd').text
     hash_tags = [el.text for el in soup.select('.link.tag')]
-    return {'price': price, 'hash_tags': hash_tags}
+    re_search_id = re.search(r'showImportantLogs\((\d+)\);', str(soup.select_one('#important-logs')))
+    id_skladchina = re_search_id.group(1)
+    return {'price': price, 'hash_tags': hash_tags, 'id': id_skladchina}
 
 
 def get_rubrics() -> List[dict]:
@@ -217,20 +256,34 @@ def get_rubrics() -> List[dict]:
     return rubrics
 
 
-def get_skladchini(rubric_url: str) -> List[dict]:
+def get_skladchini(rubric_url: str, first_page=1, max_page=None, sort=False) -> List[dict]:
     """
     Получение складчин со страници рубрики
     складчина - {'наименованиее данных':'данные'}
     """
-    response = get_response(rubric_url)
-    max_page = parse_max_page(response)
-    skladchini = parse_skladchini(response)
-    print(f'[INFO] Страница 1/{max_page}')
-    yield skladchini
+    if sort:
+        response = get_response(rubric_url + '?order=post_date')
+    else:
+        response = get_response(rubric_url)
+    parsed_max_page = parse_max_page(response)
+    if not max_page:
+        max_page = parsed_max_page
+    else:
+        if parsed_max_page < max_page:
+            max_page = parsed_max_page
+    if first_page == 1:
+        skladchini = parse_skladchini(response)
+        print(f'[INFO] Страница 1/{max_page}')
+        yield skladchini
     if max_page > 1:
-        for page in range(2, max_page + 1):
+        for page in range(first_page, max_page + 1):
+            if page == 1:
+                continue
             print(f'[INFO] Страница {page}/{max_page}')
-            url_page = rubric_url + f'page-{page}'
+            if sort:
+                url_page = rubric_url + f'page-{page}?order=post_date'
+            else:
+                url_page = rubric_url + f'page-{page}'
             response_page = get_response(url_page)
             skladchini_from_page = parse_skladchini(response_page)
             yield skladchini_from_page
@@ -243,6 +296,10 @@ def get_skladchina(skladchina_url: str) -> dict:
     """
     response = get_response(skladchina_url)
     skladchina = parse_skladchina(response)
+    event_url = 'https://www.skladchina.biz/threads/showLog?shopping_id={}&important=0'.format(skladchina['id'])
+    response_events = get_response(event_url)
+    event = parse_event(response_events)
+    skladchina['event'] = event
     return skladchina
 
 
@@ -252,14 +309,18 @@ def get_rubric_name(rubric_url: str) -> str:
     return soup.select_one('h1').text
 
 
-def parser(url_rubric, rubric_name, excel):
-    skladchini = get_skladchini(url_rubric)
+def parser(url_rubric, rubric_name, excel, first_page=1, max_page=None, sort=False, delta_days=None):
+    skladchini = get_skladchini(url_rubric, first_page, max_page, sort)
     for skladchini_from_page in skladchini:
         for index, skladchina in enumerate(skladchini_from_page):
             skladchina_detail = get_skladchina(skladchina['url'])
             skladchina['rubric'] = rubric_name
             skladchina_full = {**skladchina, **skladchina_detail}
-            print('[INFO] {} {}/{}'.format(rubric_name, index, len(skladchini_from_page)))
+            if delta_days:
+                if (datetime.now() - skladchina_full['date']).days > delta_days:
+                    excel.save()
+                    return
+            print('{} {}/{}'.format(rubric_name, index, len(skladchini_from_page)))
             excel.add_skladchina(skladchina_full)
         excel.save()
 
@@ -269,39 +330,107 @@ def parser_1():
     Парсер 1-й режим:
     парсер всего форума
     """
-    rubrics = get_rubrics()
     excel = db.Excel()
+    rubrics = get_rubrics()
     for rubric in rubrics:
         excel.file_name = rubric['name'] + '.xlsx'
         excel.create_file()
-        parser(rubric['url'], rubric['name'], excel)
+        parser(rubric['url'], rubric['name'], excel=excel)
 
 
 def parser_2():
     """
     Парсер 2-й режим:
-    парсер одной рубрики по ссылке
+    парсер одной рубрики по ссылке с сортировкой по дате и ограницением по страницам
     """
     url_rubric = input('Введите url рубрики: ')
-    rubric_name = get_rubric_name(url_rubric)
     excel = db.Excel()
+    rubric_name = get_rubric_name(url_rubric)
     excel.file_name = rubric_name + '.xlsx'
     excel.create_file()
-    parser(url_rubric, rubric_name, excel)
+    parser(url_rubric, rubric_name, excel=excel)
 
 
-if __name__ == '__main__':
+def parser_3():
+    """
+    Парсер 3-й режим:
+    парсер одной рубрики по ссылке с сортировкой по дате и ограницением по страницам
+    """
+    url_rubric = input('Введите url рубрики: ')
+    max_page = int(input('Введите максимальное кол-во страниц: '))
+    rubric_name = get_rubric_name(url_rubric)
+    excel = db.Excel()
+    excel.file_name = f'{rubric_name} 1-{max_page}.xlsx'
+    excel.create_file()
+    parser(url_rubric, rubric_name, max_page=max_page, sort=True, excel=excel)
+
+
+def parser_4():
+    """
+    Парсер 4-й режим:
+    парсер одной рубрики по ссылке с сортировкой
+    по дате и по определённому диапозону страниц
+    """
+    url_rubric = input('Введите url рубрики: ')
+    first_page = int(input('Введите первую страницу: '))
+    last_page = int(input('Введите последнюю страницу: '))
+    rubric_name = get_rubric_name(url_rubric)
+    excel = db.Excel()
+    excel.file_name = f'{rubric_name} {first_page}-{last_page}.xlsx'
+    excel.create_file()
+    parser(url_rubric, rubric_name, first_page=first_page, max_page=last_page, sort=True, excel=excel)
+
+
+def parser_5(delta_days):
+    """
+    Парсер 5-й режим:
+    парсер всего форума по диапозону времени
+    """
+    excel = db.Excel()
+    rubrics = get_rubrics()
+    for rubric in rubrics:
+        excel.file_name = rubric['name'] + f'_{delta_days}_days.xlsx'
+        excel.create_file()
+        parser(rubric['url'], rubric['name'], excel=excel, sort=True, delta_days=delta_days)
+
+
+def start():
+    email = input('Введите email: ')
+    password = input('Введите password: ')
+    login(email, password)
     info = """Режимы работы:
     1 - парсер всего форума
     2 - парсер одной рубрики по ссылке
-    """
+    3 - парсер одной рубрики по ссылке с сортировкой 
+        по дате и ограницением по страницам
+    4 - парсер одной рубрики по ссылке с сортировкой 
+        по дате и по определённому диапозону страниц
+    5 - парсер всего форума за период вермени"""
+    additional_mod_info = """   Доп. Настройка:
+        1 - Пол года 
+        2 - Месяц
+        3 - Неделя"""
     print(info)
-    mode = input('Введите режим работы (1/2): ')
-    try:
-        if int(mode) == 1:
-            parser_1()
-        elif int(mode) == 2:
-            parser_2()
-    except Exception:
-        print(traceback.format_exc())
-        input('Нажмите любую кнопку')
+    mode = input('Введите режим работы (1/2/3/4/5): ')
+    if int(mode) == 1:
+        parser_1()
+    elif int(mode) == 2:
+        parser_2()
+    elif int(mode) == 3:
+        parser_3()
+    elif int(mode) == 4:
+        parser_4()
+    elif int(mode) == 5:
+        print(additional_mod_info)
+        additional_mod = input('Доп. настройка(1/2/3):')
+        if int(additional_mod) == 1:
+            parser_5(182)
+        elif int(additional_mod) == 2:
+            parser_5(30)
+        elif int(additional_mod) == 3:
+            parser_5(7)
+
+
+if __name__ == '__main__':
+    start()
+
